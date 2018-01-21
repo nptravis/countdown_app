83427622c2252f253e3436dc5535942845b2e04e
nptravis <nptravis@yahoo.com>
2018-01-21 11:46:22 +0700

    making progress on new countdown app

--- new_app/application.py
+++ new_app/application.py
@@ -1,15 +1,23 @@
 import os
-from flask import Flask, render_template, flash, url_for, request, session, redirect
-# from flask_session import Session 
-# from passlib.apps import custom_app_context as pwd_context
-# from tempfile import mkdtemp
-# import datetime
+from flask import Flask, render_template, jsonify, flash, url_for, request, session, redirect
+from flask_session import Session 
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+from datetime import datetime
+import time
 import sqlite3
+from flask_jsglue import JSGlue
 
-# from helpers import *
+from helpers import *
 
 # instantiate flask app object
 app = Flask(__name__)
+app.config['DEBUG'] = True
+JSGlue(app)
+
+# create connection to database
+conn = sqlite3.connect('countdown.db')
+c = conn.cursor()
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
@@ -19,6 +27,12 @@ if app.config["DEBUG"]:
         response.headers["Expires"] = 0
         response.headers["Pragma"] = "no-cache"
         return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.context_processor
 def override_url_for():
@@ -33,22 +47,125 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
-@app.route("/")
-@app.route("/index")
-def index():
-	return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("layout.html")
+    if request.method == "POST":
+        return redirect(url_for("register"))
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    # forget any user_id
+    session.clear()
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+
+        # ensure email was submitted
+        if not request.form.get('email'):
+            return render_template("login.html")
+
+        # ensure password was submitted
+        elif not request.form.get('password'):
+            return render_template("login.html")
+
+        # query database for email
+        rows = c.execute("SELECT * FROM user_data WHERE user_email = :email", {'email': request.form.get('email')} ).fetchall()
+
+        # ensure username exists and password matches the hash stored
+        if len(rows) != 1:
+            error_message = "Sorry, users doesn't exist"
+            return render_template('apology.html', error_message=error_message)
+
+        elif not pwd_context.verify(request.form.get("password"), rows[0][2]):
+            error_message = "Sorry, password didn't match"
+            return render_template('apology.html', error_message=error_message)
+
+        # remember which user has logged in
+        session["user_id"] = rows[0][0]
+        print(session["user_id"])
+
+        # redirect to index page
+        return redirect(url_for("index"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user."""
-    # session.clear()
-
     if request.method == "GET":
-    	return render_template("register.html")
+        return render_template("register.html")
 
     if request.method == "POST":
-    	return render_template("register.html")
+        
+        # make sure password confirmation matches
+        if request.form.get('password') != request.form.get('confirmation'):
+            print("password and confirmation don't match")
+            return render_template("login.html")
+
+        pass_hash = pwd_context.encrypt(request.form.get('password'))
+        
+        with conn:
+            c.execute("INSERT INTO user_data (user_name, user_email, hash) VALUES(:user_name, :user_email, :hash)", {'user_name': request.form.get('user_name'), 'user_email': request.form.get("email"), 'hash': pass_hash,})
+
+        # log the user into the session
+        session["user_id"] = c.execute("SELECT user_id FROM user_data WHERE user_email=:email", {'email': request.form.get("email"),}).fetchone()
+
+        if not session["user_id"]:
+            error_message = "Sorry, problem logging in, please fix your code!"
+            return render_template('apology.html', error_message=error_message)
+
+        return redirect(url_for('create'))
+
+@app.route("/create", methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        # set inputs as variables, then put in an array
+        finish_date = request.form.get("date") + " " + request.form.get("time")
+        created_date = datetime.now().strftime("%Y-%m-%d %H:%M")
+        genre = request.form.get("genre")
+        
+        with conn:
+            c.execute("INSERT INTO user_apps ( user_id, created_date, finish_date, genre) VALUES (:user_id, :created_date, :finish_date, :genre)", {
+                'user_id': session["user_id"],
+                'created_date': created_date,
+                'finish_date': finish_date,  
+                'genre': genre 
+                });
+
+        return redirect(url_for('index'))
+
+@app.route("/index")
+@login_required
+def index():
+
+    # query database for user name
+    user_name = c.execute("SELECT user_name FROM user_data WHERE user_id=:user_id", {'user_id': session["user_id"]}).fetchone()[0]
+    
+    return render_template("index.html", user_name=user_name)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
+
+
+@app.route("/showApps")
+def showApps():
+
+    # query database for users countdowns
+    user_apps = c.execute("SELECT * FROM user_apps WHERE user_id=:user_id", {'user_id': session["user_id"]}).fetchall()
+
+    return jsonify(user_apps)
+
+
+
 
 
 
--- new_app/static/scripts.js
+++ new_app/static/scripts.js
@@ -1,1 +1,85 @@
-console.log("hello World");
+var deadline = 'January 31 2018 18:50:00 UTC+0700';
+$(document).ready(function(){
+	
+	if(top.location.pathname == '/index'){
+			
+			$.getJSON(Flask.url_for("showApps")).done(function(data){
+				console.log(data.length)
+				
+				// initialize html for clock for every app
+				for(var i = 0; i < data.length; i++){
+					var htmlString = 
+							'<div class="row well">'+
+								'<div class="col-sm-2">'	+
+									'<h3>Days<hr></h3>'+
+									'<div id="day"></div>'+
+								'</div>'+
+								'<div class="col-sm-2">'+
+									'<h3>Hours<hr></h3>'+
+									'<div id="hour"></div>'+
+								'</div>'+
+								'<div class="col-sm-2">'+
+									'<h3>Minutes<hr></h3>'+
+									'<div id="min"></div>'+
+								'</div>'+
+								'<div class="col-sm-2">'+
+									'<h3>Seconds<hr></h3>'+
+									'<div id="sec"></div>'+
+							'</div>';
+						document.getElementById("appDiv").innerHTML = htmlString;
+					
+					// set time remaining inside the appDiv
+					console.log(data[i][2])
+					initializeClock(data[i][2]);
+				}
+				
+			});
+		
+	}
+
+	function getTimeRemaining(endtime){
+	  var t = Date.parse(endtime) - Date.parse(new Date());
+	  var seconds = Math.floor( (t/1000) % 60 );
+	  var minutes = Math.floor( (t/1000/60) % 60 );
+	  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	  var days = Math.floor( t/(1000*60*60*24) );
+	   return {
+	    'total': t,
+	    'days': days,
+	    'hours': hours,
+	    'minutes': minutes,
+	    'seconds': seconds
+  		}
+	}
+
+	function initializeClock(endtime){
+	  var days = document.getElementById("day");
+	  var hours = document.getElementById("hour");
+	  var minutes = document.getElementById("min");
+	  var seconds = document.getElementById("sec");
+
+	  function updateClock(){
+	  var t = getTimeRemaining(endtime);
+	  days.innerHTML = "<h2>" + t.days + " :" + "</h2>";
+	  hours.innerHTML = "<h2>" + t.hours + " :" + "</h2>";
+	  minutes.innerHTML = "<h2>" + t.minutes + " :" + "</h2>";
+	  seconds.innerHTML = "<h2>" + t.seconds + "</h2>";
+  
+  
+  	if(t.total<=0){
+    	clearInterval(timeinterval);
+  	}
+	}
+
+
+		updateClock(); // run function once at first to avoid delay
+		var timeinterval = setInterval(updateClock,1000);
+	}
+	 
+});
+
+
+
+// localStorage.setItem('text', fieldValue);
+// localStorage.getItem('text');
+// localStorage.removeItem('text');
